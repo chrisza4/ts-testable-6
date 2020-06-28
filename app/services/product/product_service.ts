068bc0a44cbadd5fafafadd4707000a0f666d237
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2020-06-28 10:51:31 +0700

    Make it clearer

--- app/services/product/product_service.ts
+++ app/services/product/product_service.ts
@@ -1,4 +1,10 @@
 import * as ProductData from './product_data'
+import { Product } from './product_type'
 
-export const getById = ProductData.getById
-export const insert = ProductData.insert
+export function getById (id: string): Promise<Product | null> {
+  return ProductData.getById(id)
+}
+
+export function insert (product: Product): Promise<Product> {
+  return ProductData.insert(product)
+}
